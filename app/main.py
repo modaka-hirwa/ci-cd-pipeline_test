2e5b906262777b8905b8b87fb7afe035de86bd31
Hirwa innocent <hirwa6316@gmail.com>
2024-11-24 17:50:05 +0200

    Add CI/CD pipeline

--- app/main.py
+++ app/main.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello, world!"
+    return "Hello, world test the changes!"
 
 if __name__== '__main__':
     app.run(debug=True)
-    
+
